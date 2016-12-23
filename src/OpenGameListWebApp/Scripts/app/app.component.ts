7bdb84b0ceecd03797d2694dddb4bb0080205bb7
Aleksandr Suchovarov <oblomingo@gmail.com>
2016-12-23 10:21:51 +0200

    Add project files.

---
+++ src/OpenGameListWebApp/Scripts/app/app.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "opengamelist",
+    template: `<h1>OpenGameList</h1><div>Work in progress...yeeeaaah!</div>`
+})
+export class AppComponent { }
